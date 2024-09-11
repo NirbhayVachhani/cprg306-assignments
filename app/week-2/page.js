13ad24c48d78b67046ec61da3124759f165b41cc
NIRBHAY ASHVINBHAI VACHHANI <nirbhayvachhani@gmail.com>
2024-09-11 17:52:18 -0600

    final

---
+++ app/week-2/page.js
@@ -0,0 +1,12 @@
+import StudentInfo from './student-info';
+
+export default function Page() {
+    return (
+    
+        <header>
+        <h1>Shopping List</h1>
+        <StudentInfo />
+        </header>
+    
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h1>Nirbhay Vachhani</h1>
+            <Link href="https://github.com/NirbhayVachhani">https://github.com/NirbhayVachhani</Link>  
+        </div>
+    );
+}
+        
