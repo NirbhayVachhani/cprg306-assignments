569cd06fe099656aea11ce204bf84ea49a1c22b2
NIRBHAY ASHVINBHAI VACHHANI <nirbhayvachhani@gmail.com>
2024-09-18 16:00:54 -0600

    Update item.js

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,7 +1,7 @@
 // item.js
 export default function Item({ name, quantity, category }) {
     return (
-      <li className="p-4 bg-gray-800 text-white">
+      <li className="p-4 bg-gray-700 text-white">
         <div>
           <h2 className="font-bold text-xl">{name}</h2>
           <p className="text-violet-400">Buy {quantity} in {category}</p>
